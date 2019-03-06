d46ee04eba9de2c9ddfda8a706d2abc784d1f027
Jishnu Mohan <jishnu7@gmail.com>
2019-03-07 02:11:52 +0530

    fix script

--- common/install.sh
+++ common/install.sh
@@ -62,7 +62,7 @@ do
   ui_print " "
 
   if $VKSEL; then
-    $SELECTION=$i
+    SELECTION=$i
     break
   fi
 done
@@ -70,83 +70,84 @@ done
 ui_print " "
 ui_print "+ Installing $SELECTION..."
 
-case "$SELECTION" in
+case $SELECTION in
   "Anjali Old Lipi")
-    FONT_BOLD=AnjaliOldLipi-Regular
-    FONT_REGULAR=AnjaliOldLipi-Regular
-    FONT_UI_BOLD=AnjaliOldLipi-Regular
-    FONT_UI_REGULAR=AnjaliOldLipi-Regular
+    FONT_BOLD="AnjaliOldLipi-Regular"
+    FONT_REGULAR="AnjaliOldLipi-Regular"
+    FONT_UI_BOLD="AnjaliOldLipi-Regular"
+    FONT_UI_REGULAR="AnjaliOldLipi-Regular"
   ;;
   "Chilanka")
-    FONT_BOLD=Chilanka-Regular
-    FONT_REGULAR=Chilanka-Regular
-    FONT_UI_BOLD=Chilanka-Regular
-    FONT_UI_REGULAR=Chilanka-Regular
+    FONT_BOLD="Chilanka-Regular"
+    FONT_REGULAR="Chilanka-Regular"
+    FONT_UI_BOLD="Chilanka-Regular"
+    FONT_UI_REGULAR="Chilanka-Regular"
   ;;
   "Dyuthi")
-    FONT_BOLD=Dyuthi-Regular
-    FONT_REGULAR=Dyuthi-Regular
-    FONT_UI_BOLD=Dyuthi-Regular
-    FONT_UI_REGULAR=Dyuthi-Regular
+    FONT_BOLD="Dyuthi-Regular"
+    FONT_REGULAR="Dyuthi-Regular"
+    FONT_UI_BOLD="Dyuthi-Regular"
+    FONT_UI_REGULAR="Dyuthi-Regular"
   ;;
   "Gayathri")
-    FONT_BOLD=Gayathri-Bold
-    FONT_REGULAR=Gayathri-Regular
-    FONT_UI_BOLD=Gayathri-Bold
-    FONT_UI_REGULAR=Gayathri-Regular
+    FONT_BOLD="Gayathri-Bold"
+    FONT_REGULAR="Gayathri-Regular"
+    FONT_UI_BOLD="Gayathri-Bold"
+    FONT_UI_REGULAR="Gayathri-Regular"
+  ;;
   "Gayathri + Manjari")
-    FONT_BOLD=Manjari-Bold
-    FONT_REGULAR=Manjari-Regular
-    FONT_UI_BOLD=Gayathri-Bold
-    FONT_UI_REGULAR=Gayathri-Regular
+    FONT_BOLD="Manjari-Bold"
+    FONT_REGULAR="Manjari-Regular"
+    FONT_UI_BOLD="Gayathri-Bold"
+    FONT_UI_REGULAR="Gayathri-Regular"
   ;;
   "Karumbi")
-    FONT_BOLD=Karumbi-Regular
-    FONT_REGULAR=Karumbi-Regular
-    FONT_UI_BOLD=Karumbi-Regular
-    FONT_UI_REGULAR=Karumbi-Regular
+    FONT_BOLD="Karumbi-Regular"
+    FONT_REGULAR="Karumbi-Regular"
+    FONT_UI_BOLD="Karumbi-Regular"
+    FONT_UI_REGULAR="Karumbi-Regular"
   ;;
   "Keraleeyam")
-    FONT_BOLD=Keraleeyam-Regular
-    FONT_REGULAR=Keraleeyam-Regular
-    FONT_UI_BOLD=Keraleeyam-Regular
-    FONT_UI_REGULAR=Keraleeyam-Regular
+    FONT_BOLD="Keraleeyam-Regular"
+    FONT_REGULAR="Keraleeyam-Regular"
+    FONT_UI_BOLD="Keraleeyam-Regular"
+    FONT_UI_REGULAR="Keraleeyam-Regular"
   ;;
   "Manjari")
-    FONT_BOLD=Manjari-Bold
-    FONT_REGULAR=Manjari-Regular
-    FONT_UI_BOLD=Manjari-Bold
-    FONT_UI_REGULAR=Manjari-Regular
+    FONT_BOLD="Manjari-Bold"
+    FONT_REGULAR="Manjari-Regular"
+    FONT_UI_BOLD="Manjari-Bold"
+    FONT_UI_REGULAR="Manjari-Regular"
   ;;
   "Meera")
-    FONT_BOLD=Meera-Regular
-    FONT_REGULAR=Meera-Regular
-    FONT_UI_BOLD=Meera-Regular
-    FONT_UI_REGULAR=Meera-Regular
+    FONT_BOLD="Meera-Regular"
+    FONT_REGULAR="Meera-Regular"
+    FONT_UI_BOLD="Meera-Regular"
+    FONT_UI_REGULAR="Meera-Regular"
   ;;
   "Rachana")
-    FONT_BOLD=Rachana-Regular
-    FONT_REGULAR=Rachana-Regular
-    FONT_UI_BOLD=Rachana-Regular
-    FONT_UI_REGULAR=Rachana-Regular
+    FONT_BOLD="Rachana-Regular"
+    FONT_REGULAR="Rachana-Regular"
+    FONT_UI_BOLD="Rachana-Regular"
+    FONT_UI_REGULAR="Rachana-Regular"
   ;;
   "Raghu Malayalam")
-    FONT_BOLD=aRaghuMalayalamSans-Regular
-    FONT_REGULAR=RaghuMalayalamSans-Regular
-    FONT_UI_BOLD=RaghuMalayalamSans-Regular
-    FONT_UI_REGULAR=RaghuMalayalamSans-Regular
+    FONT_BOLD="aRaghuMalayalamSans-Regular"
+    FONT_REGULAR="RaghuMalayalamSans-Regular"
+    FONT_UI_BOLD="RaghuMalayalamSans-Regular"
+    FONT_UI_REGULAR="RaghuMalayalamSans-Regular"
   ;;
   "Suruma")
-    FONT_BOLD=Suruma-Regular
-    FONT_REGULAR=Suruma-Regular
-    FONT_UI_BOLD=Suruma-Regular
-    FONT_UI_REGULAR=Suruma-Regular
+    FONT_BOLD="Suruma-Regular"
+    FONT_REGULAR="Suruma-Regular"
+    FONT_UI_BOLD="Suruma-Regular"
+    FONT_UI_REGULAR="Suruma-Regular"
   ;;
   "Uroob")
-    FONT_BOLD=Uroob-Regular
-    FONT_REGULAR=Uroob-Regular
-    FONT_UI_BOLD=Uroob-Regular
-    FONT_UI_REGULAR=Uroob-Regular
+    FONT_BOLD="Uroob-Regular"
+    FONT_REGULAR="Uroob-Regular"
+    FONT_UI_BOLD="Uroob-Regular"
+    FONT_UI_REGULAR="Uroob-Regular"
   ;;
   *)
     ui_print " "
@@ -155,13 +156,9 @@ case "$SELECTION" in
 esac
 
 if [ ! -z $SELECTION ]; then
-  ui_print " Installing 1/4..."
   cp -rf $INSTALLER/fonts/$FONT_BOLD.ttf $INSTALLER/system/fonts/NotoSansMalayalam-Bold.ttf
-  ui_print " Installing 2/4..."
   cp -rf $INSTALLER/fonts/$FONT_REGULAR.ttf $INSTALLER/system/fonts/NotoSansMalayalam-Regular.ttf
-  ui_print " Installing 3/4..."
   cp -rf $INSTALLER/fonts/$FONT_UI_BOLD.ttf $INSTALLER/system/fonts/NotoSansMalayalamUI-Bold.ttf
-  ui_print " Installing 4/4..."
   cp -rf $INSTALLER/fonts/$FONT_UI_REGULAR.ttf $INSTALLER/system/fonts/NotoSansMalayalamUI-Regular.ttf
   ui_print "  Done!"
 else
